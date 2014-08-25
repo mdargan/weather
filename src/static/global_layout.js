21b956903b70c2ca5d281f00423ab0ecfdf8b8e9
Max Dargan <max.dargan@gmail.com>
2014-08-25 19:42:34 +1100

    Added cookie for tracking city

--- src/static/global_layout.js
+++ src/static/global_layout.js
@@ -1,5 +1,22 @@
 $(function() {
 
+    function get_cookie(cookie_name) {
+        var cookie = document.cookie;
+        
+        if(cookie.length != 0) {
+            var cookie_value = cookie.match(
+                '(^|;)[\s]*' + cookie_name + '=([^;]*)' );
+            return decodeURIComponent (cookie_value[2]);
+        }
+        return '' ;
+    }
+
+    function set_cookie(cookie_name, cookie_value) {
+        document.cookie = cookie_name + "=" + encodeURIComponent( cookie_value ) +
+            "; max-age=" + 60*60*24 * 365;
+    }
+
+
     function get_city_stats() {
         var city = $("#city").val();
         $.getJSON("/city_stats.json?city=" + city, function(data) {
@@ -27,9 +44,11 @@ $(function() {
     }
 
     $('#city').change(function() {
+        set_cookie("city_preset",  $("#city").val());
         get_city_stats();
     });
 
+    $("#city").val(get_cookie("city_preset"));    
     get_city_stats();
 });
 
